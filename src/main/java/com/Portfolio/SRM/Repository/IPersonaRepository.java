a4fd65ff7ce6ca4917051753a1ce52bb47d652f9
Santiago Morales <santiago.morales2003@gmail.com>
2022-12-22 11:54:29 -0300

    Modficación del backend

---
+++ src/main/java/com/Portfolio/SRM/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+package com.Portfolio.SRM.Repository;
+
+import com.Portfolio.SRM.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
