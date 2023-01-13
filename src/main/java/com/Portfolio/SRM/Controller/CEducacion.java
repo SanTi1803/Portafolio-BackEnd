9f638dd93e52d161a26ffe96f75abea43cc2ec1b
Santiago Morales <santiago.morales2003@gmail.com>
2023-01-13 17:49:34 -0300

    dockerfile

--- src/main/java/com/Portfolio/SRM/Controller/CEducacion.java
+++ src/main/java/com/Portfolio/SRM/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://srmfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
