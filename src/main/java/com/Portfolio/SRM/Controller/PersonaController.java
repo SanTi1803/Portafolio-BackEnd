9fa0d1b1b5dee287146c1ddc8d16e862d6775435
Santiago Morales <santiago.morales2003@gmail.com>
2023-02-05 14:44:23 -0300

    update2

--- src/main/java/com/Portfolio/SRM/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/SRM/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-web-di2q.onrender.com","http://localhost:4200"})
 public class PersonaController {
     
     @Autowired
