b73a287a532749da34386096ad56d4c2dd797290
Santiago Morales <santiago.morales2003@gmail.com>
2023-02-07 20:29:04 -0300

    update links

--- src/main/java/com/Portfolio/SRM/Security/Controller/AuthController.java
+++ src/main/java/com/Portfolio/SRM/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = {"https://portfolio-web-di2q.onrender.com","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
